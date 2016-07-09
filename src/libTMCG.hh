b5be7aae2311c1092b7805dfcfdabd913fb8ed58
Heiko Stamer <HeikoStamer@gmx.net>
2016-07-09 17:33:14 +0200

    needed higher versions of libgcrypt and libgpg-error for HMAC API

--- src/libTMCG.hh
+++ src/libTMCG.hh
@@ -87,7 +87,7 @@
 	
 	#ifndef TMCG_LIBGCRYPT_VERSION
 		/* Define the necessary version number of the GNU gcrypt library */
-		#define TMCG_LIBGCRYPT_VERSION "1.2.0"
+		#define TMCG_LIBGCRYPT_VERSION "1.6.0"
 	#endif
 	
 	#ifndef TMCG_LIBGMP_VERSION
