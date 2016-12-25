be9ab70dc66e0fb36f1146cef2e25b2dfbfaa940
Heiko Stamer <HeikoStamer@gmx.net>
2016-12-25 17:36:34 +0100

    adjusted timeouts again

--- src/aiounicast.hh
+++ src/aiounicast.hh
@@ -39,9 +39,9 @@ class aiounicast
 	public:
 		static const time_t	aio_timeout_none		= 0;
 		static const time_t	aio_timeout_very_short		= 1;
-		static const time_t	aio_timeout_short		= 5;
-		static const time_t	aio_timeout_long		= 45;
-		static const time_t	aio_timeout_very_long		= 150;
+		static const time_t	aio_timeout_short		= 15;
+		static const time_t	aio_timeout_long		= 90;
+		static const time_t	aio_timeout_very_long		= 180;
 		static const size_t	aio_scheduler_none		= 0;
 		static const size_t	aio_scheduler_roundrobin	= 1;
 		static const size_t	aio_scheduler_random		= 2;
