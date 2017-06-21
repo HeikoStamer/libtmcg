ee808ac822e245b85d9e28ac44d3ac1d5b8e87ae
Heiko Stamer <HeikoStamer@gmx.net>
2017-06-21 22:46:06 +0200

    added stub for ZNPFFA

---
+++ src/CramerDamgardZNPFFA.cc
@@ -0,0 +1,103 @@
+/*******************************************************************************
+   CramerDamgardZNPFFA.cc,
+                 |Z|ero-|K|nowledge |P|roofs for |F|inite |F|ield |A|rithmetic
+
+
+     [CD98] Ronald Cramer and Ivan Damgard: 'Zero Knowledge Proofs for Finite
+       Field Arithmetic, or: Can Zero-Knowledge Be for Free?',
+     Advances in Cryptology - CRYPTO '98, LNCS 1462, pp. 424--441, 1998. 
+
+   This file is part of LibTMCG.
+
+ Copyright (C) 2017  Heiko Stamer <HeikoStamer@gmx.net>
+
+   LibTMCG is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   LibTMCG is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with LibTMCG; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+*******************************************************************************/
+
+// include headers
+#ifdef HAVE_CONFIG_H
+	#include "libTMCG_config.h"
+#endif
+#include "CramerDamgardZNPFFA.hh"
+
+CramerDamgardZNPFFA::CramerDamgardZNPFFA
+	(const size_t n_in, const size_t t_in, const size_t i_in,
+	mpz_srcptr p_CRS, mpz_srcptr q_CRS, mpz_srcptr g_CRS, mpz_srcptr h_CRS,
+	const unsigned long int fieldsize,
+	const unsigned long int subgroupsize,
+	const std::string label_in):
+			F_size(fieldsize), G_size(subgroupsize),
+			label(label_in),
+			n(n_in), t(t_in), i(i_in)
+{
+	mpz_init_set(p, p_CRS), mpz_init_set(q, q_CRS), mpz_init_set(g, g_CRS), mpz_init_set(h, h_CRS);
+	// Do the precomputation for the fast exponentiation.
+	fpowm_table_g = new mpz_t[TMCG_MAX_FPOWM_T]();
+	fpowm_table_h = new mpz_t[TMCG_MAX_FPOWM_T]();
+	mpz_fpowm_init(fpowm_table_g), mpz_fpowm_init(fpowm_table_h);
+	mpz_fpowm_precompute(fpowm_table_g, g, p, mpz_sizeinbase(q, 2L));
+	mpz_fpowm_precompute(fpowm_table_h, h, p, mpz_sizeinbase(q, 2L));
+}
+
+CramerDamgardZNPFFA::CramerDamgardZNPFFA
+	(std::istream &in,
+	const unsigned long int fieldsize,
+	const unsigned long int subgroupsize,
+	const std::string label_in):
+			F_size(fieldsize), G_size(subgroupsize),
+			label(label_in),
+			n(0), t(0), i(0)
+{
+	std::string value;
+
+	mpz_init(p), mpz_init(q), mpz_init(g), mpz_init(h);
+	in >> p >> q >> g >> h;
+	std::getline(in, value);
+	std::stringstream(value) >> n;
+	if (n > TMCG_MAX_DKG_PLAYERS)
+		n = TMCG_MAX_DKG_PLAYERS;
+	std::getline(in, value);
+	std::stringstream(value) >> t;
+	if (t > n)
+		t = n;
+	std::getline(in, value);
+	std::stringstream(value) >> i;
+	if (i >= n)
+		i = 0;
+
+	// Do the precomputation for the fast exponentiation.
+	fpowm_table_g = new mpz_t[TMCG_MAX_FPOWM_T]();
+	fpowm_table_h = new mpz_t[TMCG_MAX_FPOWM_T]();
+	mpz_fpowm_init(fpowm_table_g), mpz_fpowm_init(fpowm_table_h);
+	mpz_fpowm_precompute(fpowm_table_g, g, p, mpz_sizeinbase(q, 2L));
+	mpz_fpowm_precompute(fpowm_table_h, h, p, mpz_sizeinbase(q, 2L));
+}
+
+std::string CramerDamgardZNPFFA::Label
+	() const
+{
+	return label;
+}
+
+// TODO
+
+CramerDamgardZNPFFA::~CramerDamgardZNPFFA
+	()
+{
+	mpz_clear(p), mpz_clear(q), mpz_clear(g), mpz_clear(h);
+	mpz_fpowm_done(fpowm_table_g), mpz_fpowm_done(fpowm_table_h);
+	delete [] fpowm_table_g, delete [] fpowm_table_h;
+}
+
