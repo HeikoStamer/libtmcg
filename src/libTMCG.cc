c0d61ffeb180d8e960d8fa941ee723b4dfe335e7
Heiko Stamer <HeikoStamer@gmx.net>
2018-01-26 21:46:31 +0100

    forgotten change

--- src/libTMCG.cc
+++ src/libTMCG.cc
@@ -3,7 +3,7 @@
 
    This file is part of LibTMCG.
 
- Copyright (C) 2004, 2005, 2007, 2016, 2017  Heiko Stamer <HeikoStamer@gmx.net>
+ Copyright (C) 2004, 2005, 2007, 2016--2018  Heiko Stamer <HeikoStamer@gmx.net>
 
    LibTMCG is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
