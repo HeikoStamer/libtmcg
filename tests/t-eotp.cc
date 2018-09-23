c1af4e29b1365fe59b928955f9d1568e312c6173
Heiko Stamer <HeikoStamer@gmx.net>
2018-09-23 19:44:10 +0200

    added exception handling and cosmetic changes

--- tests/t-eotp.cc
+++ tests/t-eotp.cc
@@ -26,6 +26,7 @@
 
 #ifdef FORKING
 
+#include <exception>
 #include <sstream>
 #include <vector>
 #include <cassert>
@@ -54,107 +55,132 @@ int main
 	{
 		if (pid == 0)
 		{
-			// BEGIN child code: Sender
-			ipipestream *pipe_in = new ipipestream(pipe1fd[0]);
-			opipestream *pipe_out = new opipestream(pipe2fd[1]);
-			std::vector<mpz_ptr> M;
-			
-			// create the public messages
-			for (size_t i = 0; i < N; i++)
+			try
 			{
-				mpz_ptr tmp = new mpz_t();
-				mpz_init_set_ui(tmp, i);
-				M.push_back(tmp);
+				// BEGIN child code: Sender
+				ipipestream *pipe_in = new ipipestream(pipe1fd[0]);
+				opipestream *pipe_out = new opipestream(pipe2fd[1]);
+				std::vector<mpz_ptr> M;
+			
+				// create the public messages
+				for (size_t i = 0; i < N; i++)
+				{
+					mpz_ptr tmp = new mpz_t();
+					mpz_init_set_ui(tmp, i);
+					M.push_back(tmp);
+				}
+		
+				// initialize EOTP
+				NaorPinkasEOTP *eotp = new NaorPinkasEOTP(*pipe_in);	
+				assert(eotp->CheckGroup());
+
+				// start (1-out-of-2) oblivious transfer protocol
+				start_clock();
+				std::cout << "S: Send_interactive_OneOutOfTwo(...)" << std::endl;
+				assert(eotp->Send_interactive_OneOutOfTwo(M[0], M[1],
+					*pipe_in, *pipe_out));
+				stop_clock();
+				std::cout << "S: " << elapsed_time() << std::endl;
+
+				// start (1-out-of-N) oblivious transfer protocol
+				start_clock();
+				std::cout << "S: Send_interactive_OneOutOfN(...)" << std::endl;
+				assert(eotp->Send_interactive_OneOutOfN(M, *pipe_in, *pipe_out));
+				stop_clock();
+				std::cout << "S: " << elapsed_time() << std::endl;
+
+				// start optimized (1-out-of-N) oblivious transfer protocol
+				start_clock();
+				std::cout << "S: Send_interactive_OneOutOfN_optimized(...)" <<
+					std::endl;
+				assert(eotp->Send_interactive_OneOutOfN_optimized(M,
+					*pipe_in, *pipe_out));
+				stop_clock();
+				std::cout << "S: " << elapsed_time() << std::endl;
+			
+				// release
+				for (size_t i = 0; i < N; i++)
+				{
+					mpz_clear(M[i]);
+					delete [] M[i];
+				}
+				M.clear();
+				delete eotp;
+			
+				delete pipe_in, delete pipe_out;
+				exit(0);
+				// END child code: Prover
 			}
-			
-			// initialize EOTP
-			NaorPinkasEOTP *eotp = new NaorPinkasEOTP(*pipe_in);	
-			assert(eotp->CheckGroup());
-
-			// start (1-out-of-2) oblivious transfer protocol
-			start_clock();
-			std::cout << "S: eotp.Send_interactive_OneOutOfTwo(...)" << std::endl;
-			assert(eotp->Send_interactive_OneOutOfTwo(M[0], M[1], *pipe_in, *pipe_out));
-			stop_clock();
-			std::cout << "S: " << elapsed_time() << std::endl;
-
-			// start (1-out-of-N) oblivious transfer protocol
-			start_clock();
-			std::cout << "S: eotp.Send_interactive_OneOutOfN(...)" << std::endl;
-			assert(eotp->Send_interactive_OneOutOfN(M, *pipe_in, *pipe_out));
-			stop_clock();
-			std::cout << "S: " << elapsed_time() << std::endl;
-
-			// start optimized (1-out-of-N) oblivious transfer protocol
-			start_clock();
-			std::cout << "S: eotp.Send_interactive_OneOutOfN_optimized(...)" << std::endl;
-			assert(eotp->Send_interactive_OneOutOfN_optimized(M, *pipe_in, *pipe_out));
-			stop_clock();
-			std::cout << "S: " << elapsed_time() << std::endl;
-			
-			// release
-			for (size_t i = 0; i < N; i++)
+			catch (std::exception& e)
 			{
-				mpz_clear(M[i]);
-				delete [] M[i];
+				std::cerr << "exception catched with what = " << e.what() <<
+					std::endl;
+				exit(-1);
 			}
-			M.clear();
-			delete eotp;
-			
-			delete pipe_in, delete pipe_out;
-			exit(0);
-			// END child code: Prover
 		}
 		else
 		{
 			std::cout << "fork() = " << pid << std::endl;
-			// Receiver
-			ipipestream *pipe_in = new ipipestream(pipe2fd[0]);
-			opipestream *pipe_out = new opipestream(pipe1fd[1]);
-			mpz_t M;
-			mpz_init_set_ui(M, 0L);
+			try
+			{
+				// Receiver
+				ipipestream *pipe_in = new ipipestream(pipe2fd[0]);
+				opipestream *pipe_out = new opipestream(pipe1fd[1]);
+				mpz_t M;
+				mpz_init_set_ui(M, 0L);
 			
-			// initialize EOTP
-			NaorPinkasEOTP *eotp = new NaorPinkasEOTP();
-			eotp->PublishGroup(*pipe_out);
+				// initialize EOTP
+				NaorPinkasEOTP *eotp = new NaorPinkasEOTP();
+				eotp->PublishGroup(*pipe_out);
 
-			// start (1-out-of-2) oblivious transfer protocol
-			size_t sigma = tmcg_mpz_srandom_mod(2);
-			std::cout << "sigma = " << sigma << std::endl;
-			start_clock();
-			std::cout << "R: eotp.Choose_interactive_OneOutOfTwo(...)" << std::endl;
-			assert(eotp->Choose_interactive_OneOutOfTwo(sigma, M, *pipe_in, *pipe_out));
-			stop_clock();
-			std::cout << "R: " << elapsed_time() << std::endl;
-			std::cout << "M = " << M << std::endl;
-			assert(!mpz_cmp_ui(M, sigma));
+				// start (1-out-of-2) oblivious transfer protocol
+				size_t sigma = tmcg_mpz_srandom_mod(2);
+				std::cout << "sigma = " << sigma << std::endl;
+				start_clock();
+				std::cout << "R: Choose_interactive_OneOutOfTwo(...)" << std::endl;
+				assert(eotp->Choose_interactive_OneOutOfTwo(sigma, M,
+					*pipe_in, *pipe_out));
+				stop_clock();
+				std::cout << "R: " << elapsed_time() << std::endl;
+				std::cout << "M = " << M << std::endl;
+				assert(!mpz_cmp_ui(M, sigma));
 
-			// start (1-out-of-N) oblivious transfer protocol
-			sigma = tmcg_mpz_srandom_mod(N);
-			std::cout << "sigma = " << sigma << std::endl;
-			start_clock();
-			std::cout << "R: eotp.Choose_interactive_OneOutOfN(...)" << std::endl;
-			assert(eotp->Choose_interactive_OneOutOfN(sigma, N, M, *pipe_in, *pipe_out));
-			stop_clock();
-			std::cout << "R: " << elapsed_time() << std::endl;
-			std::cout << "M = " << M << std::endl;
-			assert(!mpz_cmp_ui(M, sigma));
+				// start (1-out-of-N) oblivious transfer protocol
+				sigma = tmcg_mpz_srandom_mod(N);
+				std::cout << "sigma = " << sigma << std::endl;
+				start_clock();
+				std::cout << "R: Choose_interactive_OneOutOfN(...)" << std::endl;
+				assert(eotp->Choose_interactive_OneOutOfN(sigma, N, M,
+					*pipe_in, *pipe_out));
+				stop_clock();
+				std::cout << "R: " << elapsed_time() << std::endl;
+				std::cout << "M = " << M << std::endl;
+				assert(!mpz_cmp_ui(M, sigma));
 
-			// start optimized (1-out-of-N) oblivious transfer protocol
-			sigma = tmcg_mpz_srandom_mod(N);
-			std::cout << "sigma = " << sigma << std::endl;
-			start_clock();
-			std::cout << "R: eotp.Choose_interactive_OneOutOfN_optimized(...)" << std::endl;
-			assert(eotp->Choose_interactive_OneOutOfN_optimized(sigma, N, M, *pipe_in, *pipe_out));
-			stop_clock();
-			std::cout << "R: " << elapsed_time() << std::endl;
-			std::cout << "M = " << M << std::endl;
-			assert(!mpz_cmp_ui(M, sigma));
+				// start optimized (1-out-of-N) oblivious transfer protocol
+				sigma = tmcg_mpz_srandom_mod(N);
+				std::cout << "sigma = " << sigma << std::endl;
+				start_clock();
+				std::cout << "R: Choose_interactive_OneOutOfN_optimized(...)" <<
+					std::endl;
+				assert(eotp->Choose_interactive_OneOutOfN_optimized(sigma, N, M,
+					*pipe_in, *pipe_out));
+				stop_clock();
+				std::cout << "R: " << elapsed_time() << std::endl;
+				std::cout << "M = " << M << std::endl;
+				assert(!mpz_cmp_ui(M, sigma));
 
-			// release
-			delete eotp;
+				// release
+				delete eotp;
 			
-			delete pipe_in, delete pipe_out;
+				delete pipe_in, delete pipe_out;
+			}
+			catch (std::exception& e)
+			{
+				std::cerr << "exception catched with what = " << e.what() <<
+					std::endl;
+				return -1;
+			}
 		}
 		int wstatus = 0;
 		std::cerr << "waitpid(" << pid << ")" << std::endl;
@@ -164,14 +190,15 @@ int main
 		{
 			std::cerr << "ERROR: ";
 			if (WIFSIGNALED(wstatus))
-				std::cerr << pid << " terminated by signal " << WTERMSIG(wstatus) << std::endl;
+				std::cerr << pid << " terminated by signal " <<
+					WTERMSIG(wstatus) << std::endl;
 			if (WCOREDUMP(wstatus))
 				std::cerr << pid << " dumped core" << std::endl;
-			return 1;
+			return -1;
 		}
-		close(pipe1fd[0]), close(pipe1fd[1]), close(pipe2fd[0]), close(pipe2fd[1]);
+		close(pipe1fd[0]), close(pipe1fd[1]);
+		close(pipe2fd[0]), close(pipe2fd[1]);
 	}
-
 	return 0;
 }
 
