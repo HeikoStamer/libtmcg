f757daf4e71ce56bd32fb2ad3c49ff9e31bb621a
Heiko Stamer <HeikoStamer@gmx.net>
2018-12-01 16:21:05 +0100

    added a short section on oblivious transfer protocols

--- src/NaorPinkasEOTP.cc
+++ src/NaorPinkasEOTP.cc
@@ -372,14 +372,20 @@ bool NaorPinkasEOTP::Send_interactive_OneOutOfN
 		if (!CheckElement(x) || !CheckElement(y))
 			throw false;
 		for (size_t i = 0; i < z.size(); i++)
+		{
 			if (!CheckElement(z[i]))
 				throw false;
+		}
 	
 		// sender: second move
 		for (size_t i = 0; i < z.size(); i++)
+		{
 			for (size_t j = 0; j < i; j++)
+			{
 				if (!mpz_cmp(z[i], z[j]))
 					throw false;
+			}
+		}
 		for (size_t i = 0; i < M.size(); i++)
 		{
 			// choose random $(r_i, s_i)$
@@ -470,8 +476,10 @@ bool NaorPinkasEOTP::Choose_interactive_OneOutOfN
 		for (size_t i = 0; i < N; i++)
 			in >> w[i] >> ENC[i];
 		for (size_t i = 0; i < N; i++)
+		{
 			if (!CheckElement(w[i]))
 				throw false;	// check in-subgroup property
+		}
 		mpz_powm(foo, w[sigma], b, p);
 		if (!mpz_invert(bar, foo, p))
 			throw false;
@@ -623,8 +631,10 @@ bool NaorPinkasEOTP::Choose_interactive_OneOutOfN_optimized
 		for (size_t i = 0; i < N; i++)
 			in >> w[i] >> ENC[i];
 		for (size_t i = 0; i < N; i++)
+		{
 			if (!CheckElement(w[i]))
 				throw false;	// check in-subgroup property
+		}
 		mpz_powm(foo, w[sigma], b, p);
 		if (!mpz_invert(bar, foo, p))
 			throw false;
@@ -659,3 +669,4 @@ NaorPinkasEOTP::~NaorPinkasEOTP
 	tmcg_mpz_fpowm_done(fpowm_table_g);
 	delete [] fpowm_table_g;
 }
+
