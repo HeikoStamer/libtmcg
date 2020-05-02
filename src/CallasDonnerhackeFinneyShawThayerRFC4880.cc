91eaccaed0dcfcfd0c681a3c11b3cda2bbb53692
Heiko Stamer <HeikoStamer@gmx.net>
2020-05-02 17:19:45 +0200

    added new year

--- src/CallasDonnerhackeFinneyShawThayerRFC4880.cc
+++ src/CallasDonnerhackeFinneyShawThayerRFC4880.cc
@@ -18,7 +18,7 @@
 
    This file is part of LibTMCG.
 
- Copyright (C) 2016, 2017, 2018, 2019  Heiko Stamer <HeikoStamer@gmx.net>
+ Copyright (C) 2016, 2017, 2018, 2019, 2020  Heiko Stamer <HeikoStamer@gmx.net>
 
    LibTMCG is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
